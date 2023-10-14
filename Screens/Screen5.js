508e24b9987e619185407953c309e6a97af96790
Sasanika <sasanikaruwanthi@gmail.com>
2023-10-14 12:28:30 +0530

    except terms and conditions other pages ready

--- Screens/Screen5.js
+++ Screens/Screen5.js
@@ -1,16 +1,77 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { IconButton, MD3Colors } from 'react-native-paper';
+import ToggleSwitch from 'toggle-switch-react-native';
 import 'react-native-gesture-handler';
+
+const Setting = () => {
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false); // Set to false initially
+
+  const toggleNotifications = () => {
+    setNotificationsEnabled(!notificationsEnabled);
+    // Implement logic to update notification preferences in your app.
+  };
+
+  const handleSignOut = () => {
+    // Implement logic to sign the user out of the app.
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={{ margin: 20, fontWeight: 'bold' }}>
+          Toggle notifications on or off to stay updated with the latest news and updates from our app.
+          When notifications are enabled, you'll receive timely alerts about new content and important announcements.
+        </Text>
+        <ToggleSwitch
+          isOn={notificationsEnabled}
+          onColor="blue"
+          offColor="red"
+          label="Notifications"
+          labelStyle={{ color: "black", fontWeight: "900" }}
+          size="large"
+          onToggle={toggleNotifications}
+        />
+        <Text style={{ margin: 20, fontWeight: 'bold' }}>
+          Signing out will log you out of your account and remove your access to the app.
+          You can sign in again later to continue using our services.
+        </Text>
+        <TouchableOpacity onPress={handleSignOut} style={styles.signOutButton}>
+          <Text style={styles.signOutButtonText}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 function Screen5(props) {
   return (
-    <View>
-      <Text>Screen5</Text>
-    
-    </View>
+    <Setting />
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 3,
+    backgroundColor: 'white',
+    padding: 16,
+    alignItems: 'center',
+  },
+  signOutButton: {
+    backgroundColor: 'red',
+    borderRadius: 10,
+    width: 150,
+    padding: 12,
+  },
+  signOutButtonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default Screen5;
--- Screens/Screen6.js
+++ Screens/Screen6.js
@@ -1,16 +1,123 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { IconButton, MD3Colors, Provider as PaperProvider } from 'react-native-paper';
 import 'react-native-gesture-handler';
+
+const MyProfile = () => {
+  // State to hold user information
+  const [userData, setUserData] = useState({
+    username: 'JohnDoe',
+    email: 'johndoe@example.com',
+    department: 'Engineering',
+    jobTitle: 'Software Developer',
+  });
+
+  const handleSaveButtonPress = () => {
+    console.log('Save button pressed');
+    // Add your saving logic here
+  };
+
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={require('../asset/dp.jpg')} />
+            <View style={styles.cameraIconContainer}>
+              
+            </View>
+          </View>
+          <View style={styles.textInputContainer}>
+  <Text style={styles.infoText}>
+    <Text style={styles.boldText}>Username:</Text> {userData.username}
+  </Text>
+  <Text style={styles.infoText}>
+    <Text style={styles.boldText}>Password:</Text> {userData.password}
+  </Text>
+  <Text style={styles.infoText}>
+    <Text style={styles.boldText}>Workspace Email Address:</Text> {userData.email}
+  </Text>
+  <Text style={styles.infoText}>
+    <Text style={styles.boldText}>Department Name:</Text> {userData.department}
+  </Text>
+  <Text style={styles.infoText}>
+    <Text style={styles.boldText}>Job Title:</Text> {userData.jobTitle}
+  </Text>
+</View>
+
+          <TouchableOpacity
+            style={styles.saveButton}
+            onPress={handleSaveButtonPress}
+          >
+            <Text style={styles.saveButtonText}>Edit Details</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </PaperProvider>
+  );
+};
+
 
 function Screen6(props) {
   return (
-    <View>
-      <Text>Screen6</Text>
-    
-    </View>
+    <MyProfile/>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+  cameraIconContainer: {
+    position: 'absolute',
+    right: 80,
+    bottom: 0,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  infoText: {
+    marginBottom: 10,
+    fontSize: 16,
+    borderBottomColor: 'gray',  // Change this to your desired color
+    borderBottomWidth: 2,       // Change this to your desired border width
+    fontSize: 16,
+    marginBottom: 10,
+    paddingVertical: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  saveButton: {
+    marginVertical: 15,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    borderRadius: 5,
+  },
+  saveButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
 
 export default Screen6;
