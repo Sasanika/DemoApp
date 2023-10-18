88ae9ebe2a07da7e49756dfc5f3e759504e1df07
Sasanika <sasanikaruwanthi@gmail.com>
2023-10-18 09:39:38 +0530

    QR save function added

--- Screens/Screen33.js
+++ Screens/Screen33.js
@@ -1,27 +1,61 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
-import 'react-native-gesture-handler';
+import React, { useRef, useState } from 'react';
+import {
+  Text,
+  View,
+  Button,
+  Platform,
+  PermissionsAndroid,
+  TextInput,
+} from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+import ViewShot from 'react-native-view-shot';
+import RNFS from 'react-native-fs';
 
-function Screen33(props) {
-    console.log(props);
+const Screen33 = () => {
+  const [QRCodeText, setQRCodeText] = useState('default');
+  const qrCodeRef = useRef(null);
+  const viewShotRef = useRef(null);
+
+  const handleDownloadQRCode = async () => {
+    try {
+      if (Platform.OS === 'android') {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+        );
+        if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
+          alert('Permission denied');
+          return;
+        }
+      }
+      await viewShotRef.current.capture().then(async (uri) => {
+        const path = RNFS.PicturesDirectoryPath + `/${QRCodeText}.png`;
+        await RNFS.moveFile(uri, path);
+        await RNFS.scanFile(path);
+        alert('QR code has been downloaded successfully');
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Screen33</Text>
-
-      <TouchableOpacity style={styles.button} onPress={() => props.navigation.replace('Screen34')}>
-          <Text>Let's Get Started</Text>
-      </TouchableOpacity>
-    
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ViewShot ref={viewShotRef} options={{ format: 'png', quality: 1.0 }}>
+        <QRCode
+          ref={qrCodeRef}
+          value={QRCodeText} // Use QRCodeText instead of QRCode
+          size={200}
+          color='black'
+          backgroundColor='white'
+        />
+      </ViewShot>
+      <TextInput
+        style={{ backgroundColor: 'lightgray', width: '90%', margin: 30 }}
+        onChangeText={(text) => setQRCodeText(text)} // Update state variable
+      />
+      <Button title="Download QR Code" onPress={handleDownloadQRCode} />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-    button: {
-        alignItems: 'center',
-        backgroundColor: '#DDDDDD',
-        padding: 10,
-      },
-});
+};
 
 export default Screen33;
